c288ffbb0839d1ecec4619d4885e097e1c409d74
kattylucy <kattybarroso1321@gmail.com>
2021-01-19 16:30:14 -0800

    added current user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+class User < ApplicationRecord  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :post
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_230233) do
+ActiveRecord::Schema.define(version: 2021_01_20_002007) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_01_19_230233) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
